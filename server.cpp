ea48b3bc6297e36e0700d9fea244c4b6a939b446
arushiahmed <35859311+arushiahmed@users.noreply.github.com>
2018-12-12 23:06:21 -0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -17,13 +18,13 @@ using namespace std;
 struct user {
 	string username;
 	int socket; 
-	char ip[INET_ADDRSTRLEN];
 	string curr_room;
+	char ip[INET_ADDRSTRLEN];
 };
 
 //Hashmaps to store the list of clients and the list of rooms
 map<string, user> clients_list;
-map<string, set<string> > rooms_list;
+map<string, set<string>> rooms_list;
 
 int clients[100];
 
@@ -42,9 +43,21 @@ string get_username(string message){
 void send_dm(string message, int sockno) {
     pthread_mutex_lock(&mutex);
     
-    int start = message.find(":") + 1;
+	int start = message.find(":") + 1;
+	if(message.at(start) != '\\'){
+		int end = message.find(" ", start);
+		string msg = message.substr(start, (end - start));
+		//if(msg.at(msg.length() - 1) == '\n') msg = msg.substr(0, msg.length() - 1);
+		msg += " command not recognized.\nType \\HELP for available commands."; 
+        if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
+            perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
     int next = message.find(" ", start);
-    string message_to_send = message.substr(next + 1, (message.length() - next - 2));
+    string message_to_send = message.substr(next + 1, (message.length() - next - 1));
     string other_username;
     if(next != -1) other_username = message.substr(start + 1, (next - start - 1));
     else{
@@ -53,7 +66,8 @@ void send_dm(string message, int sockno) {
     }
 
     int other_sockno = -1;
-    string username;
+	string username;
+	
     for(map<string, user>::iterator it = clients_list.begin(); it != clients_list.end(); it++){
         if(other_username.compare(it->first) == 0){
             other_sockno = it->second.socket;
@@ -61,29 +75,48 @@ void send_dm(string message, int sockno) {
         if(sockno == it->second.socket){
             username = it->first;
         }
-    }
+	}
+
+	if(username.compare(other_username) == 0){
+		string msg = "You cannot send a message to yourself."; 
+        if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
+            perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
+	for(map<string, set<string>>::iterator mit = rooms_list.begin(); mit != rooms_list.end(); mit++){
+        if(other_username.compare(mit->first) == 0){
+			other_sockno = -2;
+			set<string> s = rooms_list[other_username];
+			message_to_send = username + ": " + message_to_send;
+
+			for(set<string>::iterator it = s.begin(); it != s.end(); it++){
+				int socket = clients_list[*it].socket;
+				if(::send(socket, message_to_send.c_str(), message.length(), 0) < 0) {
+					perror("Failed To Send");
+				}
+			}
+		}
+	}
 
     if(other_sockno == -1){
-        string msg = other_username + " command not recognized."; 
+        string msg = other_username + " command not recognized.\nType \\HELP for available commands."; 
         if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
-            perror("sending failure");
-        }
+            perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
     }
 
-    else{
+    else if(other_sockno != -2){
         message_to_send = username + ": " + message_to_send;
 
-        printf("\nmessage: %s\n", message_to_send.c_str());
-        printf("username: %s\n", username.c_str());
-        printf("sockno: %d\n", sockno);
-        printf("other_username: %s\n", other_username.c_str());
-        printf("other_sockno: %d\n", other_sockno);
-
         if(::send(sockno, message_to_send.c_str(), message.length(), 0) < 0) {
-            perror("sending failure");
+            perror("Failed To Send");
         }
         if(::send(other_sockno, message_to_send.c_str(), message.length(), 0) < 0) {
-            perror("sending failure");
+            perror("Failed To Send");
         }
     }
 
@@ -94,39 +127,36 @@ void send_dm(string message, int sockno) {
 //Function when \JOIN is called, joins requested room
 void join(int sockno, string message) {
 	pthread_mutex_lock(&mutex);
-	
+
 	int start = message.find(":") + 1;
     start = message.find(" ", start) + 1;
     int end = message.find(" ", start);
     string username = message.substr(start, (end - start));
 	
-	printf("user: %s\n", username.c_str());
-
 	start = message.find(" ", end) + 1;
     end = message.find(" ", start);
 	string roomname = message.substr(start, (message.length() - start - 1));
 	
-	printf("room: %s\n", roomname.c_str());
-
 	if(rooms_list.find(roomname) == rooms_list.end()){
 		set<string> s;
 		s.insert(username);
 		rooms_list[roomname] = s;
 		clients_list[username].curr_room = roomname;
 		num_rooms++;
-
-		string msg = username + " joined room " + roomname + ".";
-		if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
-			perror("sending failure");
-		}
 	}
 
 	else{
 		rooms_list[roomname].insert(username);
 		clients_list[username].curr_room = roomname;
-		string msg = username + " joined room " + roomname + ".\n";
-		if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
-			perror("sending failure");
+	}
+
+	set<string> s = rooms_list[roomname];
+	string msg = username + " just joined the room " + roomname + ".";
+
+	for(set<string>::iterator it = s.begin(); it != s.end(); it++){
+		int socket = clients_list[*it].socket;
+		if(::send(socket, msg.c_str(), msg.length(), 0) < 0) {
+			perror("Failed To Send");
 		}
 	}
 
@@ -135,17 +165,26 @@ void join(int sockno, string message) {
 
 //Function when \ROOMS is called, prints list of existing rooms
 void print_rooms(int socket) {
-    pthread_mutex_lock(&mutex);
+	pthread_mutex_lock(&mutex);
+	if(rooms_list.size() == 0){
+		string msg = "There is no room available.";
+		if(::send(socket, msg.c_str(), msg.length(), 0) < 0) {
+			perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
 	string msg = "Rooms: ";
 	int i = 0;
 
-    for(map<string, set<string> >::iterator it = rooms_list.begin(); it != rooms_list.end(); it++){
+    for(map<string, set<string>>::iterator it = rooms_list.begin(); it != rooms_list.end(); it++){
         msg += it->first;
-        if(i++ != num_rooms - 1) msg += ", ";
-    }
+        if(i++ != rooms_list.size() - 1) msg += ", ";
+	}
 	
 	if(::send(socket, msg.c_str(), msg.length(), 0) < 0) {
-		perror("sending failure");
+		perror("Failed To Send");
 	}
 	pthread_mutex_unlock(&mutex);
 }
@@ -153,14 +192,34 @@ void print_rooms(int socket) {
 //Function when \LEAVE is called, leaves current room.
 void leave(string message, int sockno) {
 	pthread_mutex_lock(&mutex);
-	
 	string username = get_username(message);
+
+	if(clients_list[username].curr_room.compare("-") == 0){
+		string msg = "You are not in any room.";
+		if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
+			perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
+
 	string roomname = clients_list[username].curr_room;
 	rooms_list[roomname].erase(username);
+	clients_list[username].curr_room = "-";
 
-	string msg = username + " left this room.\n";
+	set<string> s = rooms_list[roomname];
+	string msg = username + " just left the room " + roomname + ".";
+
+	for(set<string>::iterator it = s.begin(); it != s.end(); it++){
+		int socket = clients_list[*it].socket;
+		if(::send(socket, msg.c_str(), msg.length(), 0) < 0) {
+			perror("Failed To Send");
+		}
+	}
+
+	msg = "GOODBYE";
 	if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
-		perror("sending failure");
+		perror("Failed To Send");
 	}
 
 	pthread_mutex_unlock(&mutex);
@@ -169,22 +228,30 @@ void leave(string message, int sockno) {
 //Function when \WHO is called, prints list of users in current room.
 void who(string message, int sockno) {
 	pthread_mutex_lock(&mutex);
+
+	string username = get_username(message);
+	if(clients_list[username].curr_room.compare("-") == 0){
+		string msg = "You are not in any room.";
+		if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
+			perror("Failed To Send");
+		}
+		pthread_mutex_unlock(&mutex);
+		return;
+	}
 	
 	string msg = "People in this room: ";
-	string username = get_username(message);
 	string roomname = clients_list[username].curr_room;
-	printf("u: %s, r: %s\n", username.c_str(), roomname.c_str());
 	
 	set<string> s = rooms_list[roomname];
 	int i = 0;
 
 	for(set<string>::iterator it = s.begin(); it != s.end(); it++){
 		msg += *it;
-		if(i != s.size() - 1) msg += ", ";
+		if(i++ != s.size() - 1) msg += ", ";
 	}
 
 	if(::send(sockno, msg.c_str(), msg.length(), 0) < 0) {
-		perror("sending failure");
+		perror("Failed To Send");
 	}
     
     pthread_mutex_unlock(&mutex);
@@ -192,16 +259,10 @@ void who(string message, int sockno) {
 
 //Function when \HELP is called, prints out all allowed commands
 void help(int socket) {
-    string msg = "Help Information:\n"
-				"\\JOIN: list off all the rooms,\n"
-				"\\ROOMS: available rooms,\n"
-				"\\LEAVE: leaving a chat,\n"
-				"\\WHO: which people are in the chat,\n"
-				"\\HELP: list of all the commands,\n"
-				"\\nickname message: personal message\n";
+	string msg = "\nHelp Information:\n\n\\JOIN: To join a room, type \\JOIN <your username> <roomname>\n\\ROOMS: available rooms\n\\LEAVE: leaving a room\n\\WHO: which people are in the room\n\\HELP: list of all the commands\n\\nickname message: To send a message to any user or a room\n(even if you are not part of it), type '\\<other person/room's username/roomname> <message>'\n";
 	pthread_mutex_lock(&mutex);
 	if(send(socket, msg.c_str(), msg.length(), 0) < 0) {
-		perror("sending failure");
+		perror("Failed To Send");
 	}
 	pthread_mutex_unlock(&mutex);
 }
@@ -212,51 +273,40 @@ void register_user(string message, int sockno){
 	user *u = new user();
 	u->socket = sockno;
 	u->username = username;
+	u->curr_room = "-";
 
     clients_list[username] = *u;
 }
 
 //Takes in each command or message a user is trying to send. 
-//When the user calls a command that is implemented, it will call the function for the command to work, else it will give them an error. 
-void check_msg(char *message, int sockno) {
-  char msgArray[500];
-  strcpy(msgArray, message);
-  char command[100];
-  for(int i = 0; i < strlen(message); i++) {
-	  if(msgArray[i] == ':') {
-		  int j = i + 1;
-		  int k = 0;
-		  while(msgArray[j] != ' ' && msgArray[j] != '\0') {
-			  command[k] = msgArray[j];
-			  j++;
-			  k++;
-		  }
-		  break;
-	  }
-  }
+//When the user calls a command that is implemented, it will call the function for the command to work.
+void check_msg(string message, int sockno) {
+	int start = message.find(":") + 1;
+	int end = message.find(" ", start);
+	string command = message.substr(start, (end - start));
+	
 
-  if(strcmp(command, "\\JOIN") == 0) {
-    join(sockno, message);
-  }
-  else if(strcmp(command, "\\ROOMS\n") == 0) {
-    print_rooms(sockno);
-  }
-  else if(strcmp(command, "\\LEAVE\n") == 0) {
-    leave(message, sockno);
-  }
-  else if(strcmp(command, "\\WHO\n") == 0) {
-    who(message, sockno);
-  }
-  else if(strcmp(command, "\\HELP\n") == 0) {
-    help(sockno);
-  }
-  else if(strcmp(command, "NEW") == 0) {
-    register_user(message, sockno);
-  }
-  else {
-    send_dm(message, sockno);
-  }
-
+	if(command.compare("\\JOIN") == 0) {
+		join(sockno, message);
+	}
+	else if(command.compare("\\ROOMS") == 0) {
+		print_rooms(sockno);
+	}
+	else if(command.compare("\\LEAVE") == 0) {
+		leave(message, sockno);
+	}
+	else if(command.compare("\\WHO") == 0) {
+		who(message, sockno);
+	}
+	else if(command.compare("\\HELP") == 0) {
+		help(sockno);
+	}
+	else if(command.compare("NEW") == 0) {
+		register_user(message, sockno);
+	}
+	else {
+		send_dm(message, sockno);
+	}
 }
 
 //The server will take in the message the client is trying to send to a private or group message
@@ -272,7 +322,7 @@ void *receive_msg(void *sock)
         memset(message, '\0', sizeof(message));
 	}
 	pthread_mutex_lock(&mutex);
-	printf("%s disconnected\n",client.ip);
+	printf("Client with IP %s Just Disconnected\n", client.ip);
 	for(int i = 0; i < num_clients; i++) {
 		if(clients[i] == client.socket) {
 			int j = i;
@@ -286,17 +336,18 @@ void *receive_msg(void *sock)
 	pthread_mutex_unlock(&mutex);
 }
 
-// The setup of the server. Binds and listening for socket and make sure it accepts the connection request. 
-// Also checks that the client is connected to the right server.
+
+
+//The server will take in the message the client is trying to send to a private or group message
 int main(int argc,char *argv[])
 {
 	if(argc > 2) {
-		perror("Too many arguments");
+		perror("Too many arguments. Usage: ./s (or your output file name) <port>\n");
 		exit(1);
 	}
 
 	if(argc == 1) {
-		perror("Include port number as argument");
+		perror("Include port number as argument. Usage: ./s (or your output file name) <port>\n");
 		exit(1);
 	}
 
@@ -306,15 +357,22 @@ int main(int argc,char *argv[])
 	int server_sock;
 	int client_sock;
 	int port;
+	int optval = 1;
 
 	socklen_t client_size;
 	pthread_t client_thread;
 
 	struct user cl;
     char ip[INET_ADDRSTRLEN];
-
+	
 	port = atoi(argv[1]);
 	server_sock = socket(AF_INET,SOCK_STREAM,0);
+
+	/* Eliminates "Address already in use" error from bind */
+	if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval, sizeof(int)) < 0){
+		return -1;
+	}
+
 	memset(server.sin_zero,'\0',sizeof(server.sin_zero));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
@@ -345,7 +403,7 @@ int main(int argc,char *argv[])
         
 		pthread_mutex_lock(&mutex);
 		inet_ntop(AF_INET, (struct sockaddr *) &client, ip, INET_ADDRSTRLEN);
-		printf("%s Connected\n", ip);
+		printf("Client with IP %s Connected\n", ip);
 		cl.socket = client_sock;
 		strcpy(cl.ip, ip);
         clients[num_clients++] = client_sock;
